c403355a7960e01238cd4e2c39b7533221a79707
dymsia <dymkov78@mail.ru>
2019-02-13 06:27:03 +0300

    Adding Eneriss Logo into dashboard help tabItems

--- server.R
+++ server.R
@@ -2,6 +2,16 @@
 
 shinyServer(function(input,output,session){
   
+  ######################## 1. Eneriss Logo for Dashboard help item:
+  #                         --------------------------
+  # vstavka logo Eneriss v dashboard help
+  output$image2 <- renderImage({
+    return(list(src = "images/Eneriss_logo.png", contentType = "image/png", align = "center"))
+  }, deleteFile = FALSE)
   
+  # vstavka logo Eneriss v dashboard help
+  output$image3 <- renderImage({
+    return(list(src = "images/Eneriss_logo.png", contentType = "image/png", align = "center"))
+  }, deleteFile = FALSE)
   
   }) # end shinyServer
