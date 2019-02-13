5ae4a070107df2c239141b9e411ddda911a02b47
dymsia <dymkov78@mail.ru>
2019-02-13 05:58:07 +0300

    Create general structure of the dashboard, by, adding siderbarMenu items:
    Dashboard help,
    Data Preparation,
    Train ML model(s),
    Results,
    About,
    Code

--- ui.R
+++ ui.R
@@ -2,31 +2,33 @@ require(shiny);
 library(shinydashboard);
 
 
-shinyUI(dashboardPage(
-
-  
-  dashboardHeader(title = "Eneriss ML demo"
- 
+shinyUI(
+  dashboardPage(
+  dashboardHeader(
+    title = "Eneriss ML demo"
                   
-  ), # konec dashboardHeader
-  
-  
-  
+                  ), # konec dashboardHeader
 
   
   dashboardSidebar(
     sidebarMenu(
-      
-    )
+      menuItem("Dashboard help", tabName = "dashboardhelp", icon = icon("dashboard")),
+      menuItem("Data Preparation", tabName = "datapreparation", icon = icon("wrench")),
+      menuItem("Train ML model(s)", tabName = "analysis", icon = icon("cogs"),
+               menuSubItem("Train Control",icon = icon("cog"), tabName = "trainvalidation"),
+               menuSubItem("Advanced tune",icon = icon("cog"), tabName = "algorithm")
+               ),
+      menuItem("Results", tabName = "results", icon = icon("dashboard")),
+      menuItem("About", tabName = "about", icon = icon("info")),
+      menuItem("Code", tabName = "code", icon = icon("code"))
+    ) # end sidebarMenu
     
-  ),
+    ), # end dashboardSideBar
+  
+  
   dashboardBody(
     
-
   ) # end dashboardBody
   
-  
-  
-) # end dashboardPage()
-
-) # end ui
+  ) # end dashboardPage()
+  ) # end ui
