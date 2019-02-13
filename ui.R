0177da6ba1c4774c8b50300c63942bfbe1381021
dymsia <dymkov78@mail.ru>
2019-02-13 05:39:08 +0300

    Adding empty shinydashboard

---
+++ ui.R
@@ -0,0 +1,32 @@
+require(shiny);
+library(shinydashboard);
+
+
+shinyUI(dashboardPage(
+
+  
+  dashboardHeader(
+ 
+                  
+  ), # konec dashboardHeader
+  
+  
+  
+
+  
+  dashboardSidebar(
+    sidebarMenu(
+      
+    )
+    
+  ),
+  dashboardBody(
+    
+
+  ) # end dashboardBody
+  
+  
+  
+) # end dashboardPage()
+
+) # end ui
